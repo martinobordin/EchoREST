c35c49934f5741f33a6e75231782d1e6266f9690
Martino Bordin <info@martinobordin.it>
2023-04-01 13:23:27 +0200

    Added custom response header

--- EchoREST/EchoResponse.cs
+++ EchoREST/EchoResponse.cs
@@ -2,11 +2,11 @@
 
 public record EchoResponse
 {
-    public Guid Identifier { get; set; } = Guid.NewGuid();
+    public Guid RequestIdentifier { get; set; } = Guid.NewGuid();
     public DateTime Date { get; set; } = DateTime.UtcNow;
     public string? Url { get; set; } = null;
     public string? Body { get; set; } = null;
-    public IHeaderDictionary? Headers { get; set; }
+    public IEnumerable<KeyValuePair<string, string?>>? Headers { get; set; }
     public string Method { get; set; } = null!;
     public PathString Path { get; set; }
     public QueryString QueryString { get; set; }
--- EchoREST/Program.cs
+++ EchoREST/Program.cs
@@ -4,34 +4,37 @@ using Microsoft.Extensions.Caching.Memory;
 using System.Text.Encodings.Web;
 using System.Text.Json;
 
+var cacheKey = "mrucache";
+var jsonSerializerOptions = new JsonSerializerOptions()
+{
+    Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
+    WriteIndented = true
+};
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddMemoryCache();
-
-
 var app = builder.Build();
 
 app.Map("history", (IMemoryCache memoryCache) =>
 {
-    var mruCache = memoryCache.GetOrCreate("mrucache", (cacheEntry) => new List<EchoResponse>())!;
-    return Results.Json(mruCache.Select(x => $"Identifier: {x.Identifier} - Date: {x.Date:s} - /{x.Method} {x.Path}"),
-            new JsonSerializerOptions()
-            {
-                Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
-                WriteIndented = true
-            });
+    var mruCache = memoryCache.GetOrCreate(cacheKey, (cacheEntry) => new List<EchoResponse>())!;
+
+    var result = mruCache.Select(x => $"Identifier: {x.RequestIdentifier} - Date: {x.Date:s} - /{x.Method} {x.Path}");
+    return Results.Json(result, jsonSerializerOptions);
 });
 
 app.Map("history/{id}", (Guid id, IMemoryCache memoryCache) =>
 {
-    var mruCache = memoryCache.GetOrCreate("mrucache", (cacheEntry) => new List<EchoResponse>())!;
-    var mruCacheItem = mruCache.First(x => x.Identifier == id);
-    return Results.Json(mruCacheItem,
-          new JsonSerializerOptions()
-          {
-              Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
-              WriteIndented = true
-          });
+    var mruCache = memoryCache.GetOrCreate(cacheKey, (cacheEntry) => new List<EchoResponse>())!;
+    
+    var result = mruCache.FirstOrDefault(x => x.RequestIdentifier == id);
+    if (result is null)
+    {
+        return Results.NotFound(id);
+    }
+
+    return Results.Json(result, jsonSerializerOptions);
 });
 
 app.Map("{*anyurl}", async (HttpRequest httpRequest, IMemoryCache memoryCache) =>
@@ -40,7 +43,7 @@ app.Map("{*anyurl}", async (HttpRequest httpRequest, IMemoryCache memoryCache) =
     {
         Url = httpRequest.GetDisplayUrl(),
         Method = httpRequest.Method,
-        Headers = httpRequest.Headers,
+        Headers = httpRequest.Headers.Select(x => new KeyValuePair<string, string?>(x.Key, x.Value)).ToList(),
         Path = httpRequest.Path,
         QueryString = httpRequest.QueryString,
         Query = httpRequest.Query
@@ -51,17 +54,19 @@ app.Map("{*anyurl}", async (HttpRequest httpRequest, IMemoryCache memoryCache) =
         echoResponse.Body = await streamReader.ReadToEndAsync();
     };
 
-    var mruCache = memoryCache.GetOrCreate("mrucache", (cacheEntry) => new List<EchoResponse>())!;
+    var mruCache = memoryCache.GetOrCreate(cacheKey, (cacheEntry) => new List<EchoResponse>())!;
     mruCache.Insert(0, echoResponse);
     mruCache = mruCache.Take(10).ToList();
-    memoryCache.Set("mrucache", mruCache);
+    memoryCache.Set(cacheKey, mruCache);
 
-    return Results.Json(echoResponse,
-        new JsonSerializerOptions()
-        {
-            Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
-            WriteIndented = true
-        });
+    var result = JsonSerializer.Serialize(echoResponse, jsonSerializerOptions);
+    return Results.Json(echoResponse, jsonSerializerOptions);
+});
+
+app.Use((context, next) =>
+{
+    context.Response.Headers.Append("X-Copyright", "EchoREST v1.0 developed by Martino Bordin");
+    return next(context);
 });
 
 app.Run();
